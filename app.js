1c736b338a2169c04b956d78e134bad956efedc5
kiethohoai <kietho.060193@gmail.com>
2024-08-06 08:38:51 +0700

    004 Setting up Express and Basic Routing

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const port = 5173;
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...');
+});
+
+app.listen(port, () => {
+  console.log(`App listening on ${port}`);
+});
