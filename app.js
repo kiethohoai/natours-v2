f074171d2f0e34f792a3f5fb4f3c46fa39d54e81
kiethohoai <kietho.060193@gmail.com>
2024-08-06 16:59:55 +0700

    020 Serving Static Files

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // todo Middleware
 app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   console.log('Hello, I am from Middleware');
   next();
-});
+}); */
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
