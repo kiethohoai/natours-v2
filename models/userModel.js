2563dd82f69ea272cd985ac29024b3b1da50efea
kiethohoai <kietho.060193@gmail.com>
2024-08-21 15:24:17 +0700

    002 Modelling Users

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,32 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email!'],
+    unique: true,
+    lowercase: true,
+    validator: [validator.isEmail, 'Please provide a valid email!']
+  },
+  photo: {
+    type: String
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password!'],
+    minlength: 8
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Password Confirm required!']
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
