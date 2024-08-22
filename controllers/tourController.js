c473ac610996e4d361636c324fede885546ce21e
kiethohoai <kietho.060193@gmail.com>
2024-08-22 10:32:52 +0700

    007 Logging in Users

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -11,34 +11,23 @@ exports.aliasTopTours = (req, res, next) => {
 };
 
 exports.getAllTours = catchAsync(async (req, res, next) => {
-                                                             // EXECUTE QUERY
-                                                             const features = new APIFeatures(
-                                                               Tour.find(),
-                                                               req.query
-                                                             )
-                                                               .filter()
-                                                               .sort()
-                                                               .limitFields()
-                                                               .paginate();
-                                                             const tours = await features.query;
+  // EXECUTE QUERY
+  const features = new APIFeatures(Tour.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const tours = await features.query;
 
-                                                             // SEND RESPONSE
-                                                             res
-                                                               .status(
-                                                                 200
-                                                               )
-                                                               .json(
-                                                                 {
-                                                                   status:
-                                                                     'success',
-                                                                   results:
-                                                                     tours.length,
-                                                                   data: {
-                                                                     tours
-                                                                   }
-                                                                 }
-                                                               );
-                                                           });
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+});
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
