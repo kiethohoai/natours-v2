28b0a82df0cc107b4d1a13d85e7a81b8917d76f7
kiethohoai <kietho.060193@gmail.com>
2024-08-23 17:33:21 +0700

    012 Password Reset Functionality Reset Token

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -39,7 +40,9 @@ const userSchema = new mongoose.Schema({
       message: 'Password are not the same!'
     }
   },
-  passwordChangedAt: Date
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetEspires: Date
 });
 
 userSchema.pre('save', async function(next) {
@@ -72,6 +75,20 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
   }
   // FALSE mean not changec!
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function() {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  console.log('🚀CHECK  resetToken =', resetToken);
+
+  this.passwordResetEspires = Date.now() + 10 * 60 * 1000;
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
