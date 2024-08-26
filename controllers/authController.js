02d08a724d2c2f22e7c2065d6181e80260b3aabc
kiethohoai <kietho.060193@gmail.com>
2024-08-26 14:49:44 +0700

    015 Updating the Current User Password

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,8 +12,18 @@ const signToken = (id) => {
   });
 };
 
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
 exports.signup = catchAsync(async (req, res, next) => {
-  // const newUser = await User.create(req.body);
   const newUser = await User.create({
     name: req.body.name,
     email: req.body.email,
@@ -23,18 +33,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     role: req.body.role,
   });
 
-  /*   const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-     expiresIn: process.env.JWT_EXPIRES_IN
-   }); */
-  const token = signToken(newUser._id);
-
-  res.status(201).json({
-    status: 'success',
-    token,
-    data: {
-      user: newUser,
-    },
-  });
+  createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -53,11 +52,7 @@ exports.login = catchAsync(async (req, res, next) => {
   }
 
   // 3: If everything is OK, send Token to the Client
-  const token = signToken(user._id);
-  res.status(200).json({
-    status: 'success',
-    token,
-  });
+  createSendToken(user, 200, res);
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -175,10 +170,25 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
   // 3 Update changedPasswordAt property for the user
   // 4 Log the user in, send JWT
-  const token = signToken(user._id);
+  createSendToken(user, 200, res);
+});
 
-  res.status(200).json({
-    status: 'success',
-    token,
-  });
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1 - Get user from collection
+  const user = await User.findById(req.user.id).select('+password');
+
+  // 2 - Check if POSTed current password is correct
+  if (
+    !(await user.correctPassword(req.body.passwordCurrent, user.password))
+  ) {
+    return next(new AppError('Your current password is wrong', 401));
+  }
+
+  // 3 - If true, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+
+  // 4 - Log user in, send JWT
+  createSendToken(user, 200, res);
 });
