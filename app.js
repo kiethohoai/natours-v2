43568903eae46c4f5f148764f5c4627e7652d8d1
kiethohoai <kietho.060193@gmail.com>
2024-08-26 21:19:50 +0700

    022 Data Sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const rateLimit = require('express-rate-limit');
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -29,6 +31,12 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data Sanitization against NoSQL injection
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS
+app.use(xss());
 
 // Serving static files!
 app.use(express.static(`${__dirname}/public`));
