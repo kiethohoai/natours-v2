d8c5dce75cd24e58066da7443d6a21b050e3d8fc
kiethohoai <kietho.060193@gmail.com>
2024-08-29 17:56:06 +0700

    013 Nested Routes with Express

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
-const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('../routes/reviewRoutes');
 
 const router = express.Router();
 
-// router.param('id', tourController.checkID);
+// POST /tour/123awfwae/reviews
+// GET /tour/123awfwae/reviews
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
@@ -27,18 +30,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour,
-  );
-
-// POST /tour/123awfwae/reviews
-// GET /tour/123awfwae/reviews
-// GET /tour/123awfwae/reviews/2348723f
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview,
   );
 
 module.exports = router;
