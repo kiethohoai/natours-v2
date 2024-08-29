0c9b126009908af3b3ea2113acd27afcf6c80a10
kiethohoai <kietho.060193@gmail.com>
2024-08-29 22:55:11 +0700

    008 Another Way of Creating Documents

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,7 +1,5 @@
 const Tour = require('../model/tourModel');
 
-// Temp tours
-
 exports.getAllTour = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -14,23 +12,30 @@ exports.getAllTour = (req, res) => {
 };
 
 exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  // const tour = tours.find(el => el.id === id);
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     tour,
-  //   },
-  // });
-};
-
-exports.createTour = (req, res) => {
   res.status(201).json({
     status: 'success',
     // data: {
     //   tour: newTour,
     // },
   });
+};
+
+// todo CREATE TOUR
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
