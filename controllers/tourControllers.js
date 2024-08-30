02f77fef1a105edee2d860235973f1e3bc343116
kiethohoai <kietho.060193@gmail.com>
2024-08-30 14:16:24 +0700

    018 Making the API Better Pagination

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -34,6 +34,19 @@ exports.getAllTour = async (req, res) => {
       query = query.select('-__v'); // Remove __v
     }
 
+    // 4) PAGINATE
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error('This page is not exist');
+      }
+    }
+
     // EXCUTE QUERY
     const tours = await query;
 
